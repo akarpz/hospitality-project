4b2028e660cf7b207854ed13819161d648ddc69e
Adam Karpowich <adameast9000@live.com>
2017-04-07 01:11:21 +0000

    updated form submission page

---
+++ form_submission.php
@@ -0,0 +1,16 @@
+<?php
+print_r($_POST . PHP_EOL);
+
+echo $_POST["todaydate"];
+echo $_POST["fname"];
+echo $_POST["lname"];
+echo $_POST["email"];
+echo $_POST["major"];
+echo $_POST["organization"];
+echo $_POST["website"];
+echo $_POST["location"]
+echo $_POST["agency"];
+echo $_POST["workdates-start"];
+echo $_POST["workdates-end"];
+e
+?>

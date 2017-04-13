59203fc66661271e68be7f7ea3479c3ba6505754
root <root@buec-hospstudent.win.udel.edu>
2017-04-13 01:26:50 -0400

    debugged database selects

--- supervisorform.php
+++ supervisorform.php
@@ -6,9 +6,10 @@ print_r($browser_url); //debug statement, remove later
 $servername = "localhost";
 $username = "root";
 $password = "=76_kill_COMMON_market_8=";
+$db_name = "hospitality-serviceform-db";
 $submission_match ="";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
@@ -17,25 +18,35 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 //prepare insert
-if (!($stmt = $mysqli->prepare("SELECT FROM submission WHERE supervisor_link = :browser_url"))) {
-     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+if (!($stmt = $conn->prepare("SELECT * FROM Submission WHERE Supervisor_Form_Link=?"))) {
+     echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;
 }
 
+echo "prepared successfully";
+
 // bind variable to parameter
-if (!$stmt->bind_param(":browser_url", $browser_url)) {
+if (!$stmt->bind_param("s", $browser_url)) {
     echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
 }
+
+echo "binded successfully";
 
 //execute
 if (!$stmt->execute()) {
     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
 
+echo "executed successfully";
+
 /* bind result variables */
 $stmt->bind_result($submission_match);
 
+echo "bind result successful";
+
 /* fetch value */
 $stmt->fetch();
+
+echo "fetch successful";
 
 /* close statement */
 $stmt->close();

74d1470034c83029fe2e89bd99a18e71460db8be
Adam Karpowich <adameast9000@live.com>
2017-04-14 01:39:45 +0000

    added db insert for student logic

--- form_submission.php
+++ form_submission.php
@@ -29,6 +29,7 @@ echo $_POST["workdates-end"];
 $servername = "localhost";
 $username = "root";
 $password = "=76_kill_COMMON_market_8=";
+$hash;
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -41,32 +42,68 @@ echo "Connected successfully";
 
 //check for student
 //if student exists, create new record. else do nothing
+check_student();
+function check_student() {
+    if ($result = $conn->query('SELECT * FROM Student WHERE UDID = "'.$_POST["id"].'"')) {
+        //then student exists
+        check_supervisor();
+        $result->close();
+    }else{ //create new student record with prepared statement
+    $newstudent = $conn->prepare('INSERT INTO Student Values (?, ?, ?, ?, ?)');
+    $newstudent->bind_param("sssss", $_POST["id"], $_POST["fname"], $_POST["lname"], $_POST["major"], $_POST["email"]);
+    $newstudent->execute();
+    check_supervisor();
+    }
+}
 
-//check for supervisor
-//if supervisor exists, create new record. else do nothing
+function check_supervisor() {
+    //check for supervisor
+    if ($result = $conn->query('SELECT * FROM Supervisor WHERE Supervisor_Email = "'.$_POST["supemail"].'"')) {
+        //then supervisor exists
+        create_submission($result["Supervisor_ID"]);
+        $result->close();
+    }else{
+        //create new supervisor record with prepared statement
+        $newsupervisor = $conn->prepare('INSERT INTO Supervisor Values(?,?,?,?,?,?,?)');
+        $newsupervisor->bind_param("sssssii", $_POST["supfname"], $_POST["suplname"], $_POST["suptitle"], $_POST["supemail"], $_POST["supphone"], $_POST["supstudent?"], $_POST["suprelative?"]);
+        $newsupervisor->execute();
+        $newsupervisorid = $conn->insert_id;
+        create_submission($newsupervisorid);
+    }
+}
+
+function create_submission($supervisor_id) {
+    $hashinput = $_POST["todaydate"] . $_POST["workdates-start"] . $_POST["workdates-end"];
+    $hash = secure($hashinput, $supervisor_id, 100);
+    //create the rest of submission
+    
+    $newsubmission = $conn->prepare('INSERT INTO Submission VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?');
+    $newsubmission->bind_param("isssssisssssis", $supervisor_id, $_POST["website"], $_POST["location"], $_POST["agency"], $_POST["workdates-start"], 
+    $_POST["workdates-end"], $_POST["hoursworked"], $_POST["activities"], $_POST["valuesite"], $_POST["valueyou"],
+     $_POST["todaydate"], $_POST["hash"]);
+    
+    $newsubmission->execute();
+    $submission_id = $conn->insert_id;
+    create_student_submission($submission_id);
+}
+
+function create_student_submission($submission_id) {
+    $statement = $conn->prepare('INSERT INTO Student_Submissions VALUES (?, ?)');
+    $statement->bind_param("ss", $_POST["id"], $submission_id);
+    $statement->execute();
+}
 
 //create hash for supervisor link
 function secure($password, $salt, $iter) {
        $temp = hash("sha256","0".$password.$salt);
-       for($i=0;$i<$iter-1;$i++){
+       for($i=0;$i<$iter-1;$i++) {
            $temp = strtoupper(hash("sha256",$temp.$password.$salt));
        }
        return $temp;
 }
 
-$submission_insert = "INSERT INTO submission VALUES ('Jane', 'Doe', 'john@example.com')";
-
-if ($conn->query($submission_insert) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $submission_insert . "<br>" . $conn->error;
-}
-
-//create submission record
-
-//create record in student_submission table
-
 $conn->close();
 
+print_r("http://serviceforms.lerner.udel.edu/supervisorform.php?ref=" . $hash);
 //redirect to page with hash, or email
 ?>

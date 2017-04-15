d2ca68a2452eb6584f0aab175e61618a07e45a13
Adam Karpowich <adameast9000@live.com>
2017-04-15 03:42:28 +0000

    added error checking for submission insert

--- form_submission.php
+++ form_submission.php
@@ -99,7 +99,9 @@ function create_submission($supervisor_id) {
     $hash = secure($hashinput, $supervisor_id, 100);
     //create the rest of submission
     
-    $newsubmission = $conn->prepare('INSERT INTO Submission VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?');
+    if(!$newsubmission = $conn->prepare('INSERT INTO Submission VALUES (0, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?')) {
+        echo "Submission Insert Prepare failed: (" . $conn->errno . ") " . $conn->error;
+    }
     $newsubmission->bind_param("isssssisssssis", $supervisor_id, $_POST["website"], $_POST["location"], $_POST["agency"], $_POST["workdates-start"], 
     $_POST["workdates-end"], $_POST["hoursworked"], $_POST["activities"], $_POST["valuesite"], $_POST["valueyou"],
     $_POST["todaydate"], $_POST["hash"]);

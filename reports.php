11d5ece4c1fd4854a5ffdfa2af52d969020c2dce
Adam Karpowich <adameast9000@live.com>
2017-05-09 23:17:38 +0000

    merging conflicts

--- reports.php
+++ reports.php
@@ -15,31 +15,24 @@ if ($conn->connect_error) {
 
 $cutoff = "06-01";
 $now = new DateTime();
-$now = $now->format('m-d');
-if($now < $cutoff) {
-    //echo "we are in the current year";
-    $yearnow = new DateTime();
-    $yearnow = $yearnow->format('y');
-    $yearone = $yearnow-1;
-    $yeartwo = $yearone-1;
-    
-    
-}
-else {
-    //echo "this is not the current year";
-    $yearnow = new DateTime();
-    $yearnow = $yearnow->format('y');
-    $yearone = $yearnow;
-    $yeartwo = $yearnow-1;
-    
-}
-$yearone = "20" . $yearone;
-$yeartwo = "20" . $yeartwo;
-$yearone =  $yearone . "-5" . "-31";
-$yeartwo =  $yeartwo . "-8" . "-25";
 
-echo "Year Two (former year): " . $yeartwo . PHP_EOL;
-echo "Year One (latter year): " . $yearone . PHP_EOL;
+if(($now->format('m-d')) < $cutoff) {
+    $previous_year_start = "20" . ($now->format('y') - 2) . "-8-25";
+    $previous_year_end = "20" . $now->format('y') - 1 . "-5-31";
+    $current_year_start = "20" . ($now->format('y') - 1) . "-8-25";
+    $current_year_end = "20" . $now->format('y') . "-8-25";
+} else {
+    $previous_year_start = "20" . ($now->format('y') - 1) . "-8-25";
+    $previous_year_end = "20" . $now->format('y') . "-5-31";
+    $current_year_start = "20" . $now->format('y') . "-8-25";
+    $current_year_end = "20" . ($now->format('y') + 1) . "-8-25";
+}
+
+echo "Start date of last academic year:  " . $previous_year_start . PHP_EOL;
+echo "End date of last academic year: " . $previous_year_end . PHP_EOL;
+
+echo "Start date of this academic year:  " . $current_year_start . PHP_EOL;
+echo "End date of this academic year: " . $current_year_end . PHP_EOL;
 
 switch($ref) {
     case "all":
@@ -49,11 +42,19 @@ switch($ref) {
                             JOIN Student_Submissions";
     break;
     
-    case "last":
+    case "submissions_last_academic_year":
         $sql_statement = "SELECT * 
                             FROM Submission 
                             WHERE Submission_Date 
-                            between '" . $yeartwo . "' and '" . $yearone . "'";
+                            between '" . $previous_year_start . "' and '" . $previous_year_end . "'";
+                        
+    break;
+    
+    case "submissions_current_academic_year":
+        $sql_statement = "SELECT * 
+                            FROM Submission 
+                            WHERE Submission_Date 
+                            between '" . $current_year_start . "' and '" . $current_year_end . "'";
                         
     break;
     
@@ -69,12 +70,42 @@ switch($ref) {
 	    $sql_statement = "Select * FROM Submission";
     break;
     
-    case "stu":
+    case "student_all":
         $sql_statement = "SELECT * FROM Student";
     break;
     
-    case "sup":
-        $sql_statement = "SELECT * FROM Supervisor";
+    case "student_current":
+        $sql_statement = "SELECT Student.UDID, Student.First_Name, Student.Last_Name, Student.Major, Student.Email, Student.Phone 
+                          FROM Student 
+                          Join Student_Submissions ON Student_Submissions.UDID = Student.UDID
+                          Join Submission ON Submission.Submission_ID = Student_Submissions.Submission_ID
+                          WHERE Submission.Date between '" . $current_year_start . "' AND '" . $current_year_end . "'";
+    break;
+    
+    case "student_previous":
+        $sql_statement = "SELECT Student.UDID, Student.First_Name, Student.Last_Name, Student.Major, Student.Email, Student.Phone 
+                          FROM Student 
+                          Join Student_Submissions ON Student_Submissions.UDID = Student.UDID
+                          Join Submission ON Submission.Submission_ID = Student_Submissions.Submission_ID
+                          WHERE Submission.Date between '" . $previous_year_start . "' AND '" . $previous_year_end . "'";
+    break;
+    
+    case "supervisor_all":
+        $sql_statement = "SELECT First_Name, Last_Name, Supervisor_Title, Supervisor_Email, Supervisor_Phone FROM Supervisor";
+    break;
+    
+    case "supervisor_current":
+        $sql_statement = "SELECT First_Name, Last_Name, Supervisor_Title, Supervisor_Email, Supervisor_Phone 
+                          FROM Supervisor
+                          JOIN Submission ON Supervisor.Supervisor_ID = Submission.Supervisor_ID
+                          WHERE Submission.Date between '" . $current_year_start . "' AND '" . $current_year_end . "'";
+    break;
+    
+    case "supervisor_previous":
+        $sql_statement = "SELECT First_Name, Last_Name, Supervisor_Title, Supervisor_Email, Supervisor_Phone 
+                          FROM Supervisor
+                          JOIN Submission ON Supervisor.Supervisor_ID = Submission.Supervisor_ID
+                          WHERE Submission.Date between '" . $previous_year_start . "' AND '" . $previous_year_end . "'";
     break;
 }
 
@@ -97,11 +128,12 @@ if ($result->num_rows > 0) {
 	    fputcsv($fp, $row);
     }
 } else {
-	echo "no results";
+	echo "There are no results that match that criteria. This is most likely because it is Summer and 
+	        the current year has just started, or the databases have been recently cleared";
 }
 
 $conn->close();
-$outputfilename = "report_" . $now . ".csv";
+$outputfilename = "report_" . $now->format('Y-m-d H:i:s') . ".csv";
 fclose($fp);
 header("Content-type: application/octet-stream");
 header("Content-Transfer-Encoding; CSV");
